cd1f209f6c71bba018e3a257044c02dd063556a6
firekoder <jasonpeterz.e1@gmail.com>
2022-10-14 02:37:21 +0100

    low level language task 4-5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
 /**
  * main - Entry
  * Return: Always 0
  */
 int main(void)
 {
-	puts("\"Programming is like buiding a multilingual puzzle");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
