208f6d1e770a1d649622ce4f988ded199fbe493f
firekoder <jasonpeterz.e1@gmail.com>
2022-10-19 03:48:03 +0100

    Task 3

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-#ifndef MAIN_H
+i#ifndef MAIN_H
 #define MAIN_H
 
 void print_alphabet(void);
@@ -13,5 +13,5 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 int _putchar(char c);
-
+int _islower(int c);
 #endif
