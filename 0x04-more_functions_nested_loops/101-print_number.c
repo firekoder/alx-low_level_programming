fdae114a2988b68e1b0f44d3723de87f9323e7e8
firekoder <jasonpeterz.e1@gmail.com>
2022-10-21 05:27:00 +0100

    Advance task

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,39 +1,21 @@
 #include "main.h"
-
 /**
- * print_number - prints a number
- * @n: Input number
+ * print_number - prints numbers
+ * @n: number to be printed
+ * Return:void
  */
 
 void print_number(int n)
 {
-	long len, res, i, temp, expo;
+	unsigned int x;
 
-	res = n;
-	expo = len =  1;
-/*Check negatives*/
-	if (res < 0)
+	x = n;
+	if (n < 0)
 	{
-		res *= -1;
-		_putchar('-');
+		_putchar(45);
+		x = -n;
 	}
-
-/**/
-	temp = res;
-	while (temp >= 10)
-	{
-		len++;
-		temp /= 10;
-	}
-
-/*Create Exponent*/
-	for (i = 1; i < len; i++)
-		expo *= 10;
-/*Main */
-	while (expo > 1)
-	{
-		_putchar((res / expo) % 10 + '0');
-		expo /= 10;
-	}
-	_putchar(res % 10 + '0');
+	if (x / 10)
+		print_number(x / 10);
+	_putchar((x % 10) + '0');
 }
